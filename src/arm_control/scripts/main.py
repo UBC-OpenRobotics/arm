51fea0c5900d7d705fcff7d811a7ae57cdda4dea
Andrew <andrewxieax1@gmail.com>
2022-03-05 13:17:58 -0800

    Revert "added gripper control"
    This reverts commit 9a709324252d42faeb879ae6380faadacfb91cbe.

--- src/arm_control/scripts/main.py
+++ src/arm_control/scripts/main.py
@@ -24,8 +24,6 @@ sample_time_out   = 60               # time out in seconds for Inverse Kinematic
 sample_attempts   = 5                # num of planning attempts 
 goal_tolerance    = 0.001
 
-gripper_group_name = "gripper"
-
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node("main", anonymous=True)
 
@@ -37,8 +35,6 @@ move_group.set_num_planning_attempts(5)
 move_group.set_goal_tolerance(goal_tolerance)
 move_group.set_max_velocity_scaling_factor(1)
 move_group.set_max_acceleration_scaling_factor(0.3)
-
-gripper_group = moveit_commander.MoveGroupCommander(gripper_group_name)
 
 
 
@@ -68,7 +64,7 @@ def go_ik(goal):
         move_group.clear_pose_targets()
     return plan[0]
 
-def go_joint(group, goal):
+def go_joint(goal):
     """
     Makes the arm's joints go to joint angles specified by goal,
     blocks until goal is reached within tolerance or exit if goal joint
@@ -79,8 +75,11 @@ def go_joint(group, goal):
     returns: void
     """
 
-    group.go(joints=goal, wait=True)
-    group.stop()
+    move_group.go(joints=goal, wait=True)
+    move_group.stop()
+
+def f(goal, tolerance):
+    current_pose = move_group.get_current_pose()
 
 
 ##############################
@@ -88,8 +87,15 @@ def go_joint(group, goal):
 ##############################
 
 while (True):
-    # print(f"Success: {go_ik(move_group.get_random_pose().pose)}")
-    go_joint(gripper_group, gripper_group.get_random_joint_values())
-
+    print(f"Success: {go_ik(move_group.get_random_pose().pose)}")
     
+
+# for i in range(1):
+#     pose_goal = geometry_msgs.msg.Pose()
+#     pose_goal.position.x = -0.3
+#     pose_goal.position.y = 0.23
+#     pose_goal.position.z = 0.45
+
+#     print(f"success: {go_ik(pose_goal)} xyz: ({pose_goal.position.x}, {pose_goal.position.y}, {pose_goal.position.z})")
+
 
