7263e3111cbb806a2c60a6cc4c6989b121ff487f
JonathanQ-Maker <jonathanqfxw@gmail.com>
2022-03-11 23:10:15 -0800

    + gitignore, + gazebo_worlds pkg

---
+++ build/gazebo_worlds/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/jonathan/Development/PythonProjects/arm/devel/.private/gazebo_worlds:$CMAKE_PREFIX_PATH"
+export PWD='/home/jonathan/Development/PythonProjects/arm/build/gazebo_worlds'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/jonathan/Development/PythonProjects/arm/devel/.private/gazebo_worlds/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/jonathan/Development/PythonProjects/arm/src/gazebo_worlds:$ROS_PACKAGE_PATH"
---
+++ devel/share/gazebo_worlds/cmake/gazebo_worldsConfig-version.cmake
@@ -0,0 +1,1 @@
+/home/jonathan/Development/PythonProjects/arm/devel/.private/gazebo_worlds/share/gazebo_worlds/cmake/gazebo_worldsConfig-version.cmake
